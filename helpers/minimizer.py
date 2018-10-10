863bd31e59721cc4c53607fa3bc21128a2e61a25
YesmynameisPerry <yesmynameisperry@gmail.com>
2018-10-10 23:59:53 +1100

    cleaned up things yo

--- helpers/minimizer.py
+++ helpers/minimizer.py
@@ -11,36 +11,37 @@ def minimize(board):
                 board[row][cell] = board[row][cell][0]
     return board
 
+def _squishSegment(board, functions, segment):
+    listOfCells = functions[0](segment, board)
+    for cell in range(9):
+        if type(listOfCells[cell] == list):
+            for possibility in listOfCells[cell]:
+                listOfOthers = [item for sublist in [listOfCells[n] for n in range(9) if n != cell] for item in sublist]
+                if possibility not in listOfOthers:
+                    listOfCells[cell] = possibility
+    return functions[1](segment, listOfCells, board)
+
+def _squashSegment(board, getFunction, segment):
+    currentSquare = getFunction(segment, board)
+    finalizedList = [currentSquare[i] for i in range(9) if type(currentSquare[i]) == str]
+    for cell in range(9):
+        if type(currentSquare[cell]) == list:
+            currentSquare[cell][:] = [item for item in currentSquare[cell] if item not in finalizedList]
+    return board
+
 # takes a board, eliminates all numbers that have been finalised in conflicting spaces
 def squash(board):
     board = deepcopy(board)
-    board = _squashSegment(board, getSquare)
-    board = _squashSegment(board, getRow)
-    board = _squashSegment(board, getColumn)
-    return board
-
-def _squashSegment(board, getFunction):
-    for square in range(9):
-        currentSquare = getFunction(square, board)
-        finalizedList = [currentSquare[i] for i in range(9) if type(currentSquare[i]) == str]
-        for cell in range(9):
-            if type(currentSquare[cell]) == list:
-                currentSquare[cell][:] = [item for item in currentSquare[cell] if item not in finalizedList]
+    for segment in range(9):
+        board = _squashSegment(board, getSquare, segment)
+        board = _squashSegment(board, getRow, segment)
+        board = _squashSegment(board, getColumn, segment)
     return board
 
 # takes a board, for each cell sets a value if it only appears as a possibility once in a row/column/square
 def squish(board):
-    count = 0
-    gets = [(getSquare, putSquare), (getColumn, putColumn), (getRow, putRow)]
-    for i in range(9):
-        for boardFunction in gets:
-            listOfCells = boardFunction[0](i, board)
-            for cell in range(9):
-                if type(listOfCells[cell] == list):
-                    for possibility in listOfCells[cell]:
-                        listOfOthers = [item for sublist in [listOfCells[n] for n in range(9) if n != cell] for item in sublist]
-                        if possibility not in listOfOthers:
-                            count += 1
-                            listOfCells[cell] = possibility
-            board = boardFunction[1](i, listOfCells, board)
+    for segment in range(9):
+        board = _squishSegment(board, (getSquare, putSquare), segment)
+        board = _squishSegment(board, (getColumn, putColumn), segment)
+        board = _squishSegment(board, (getRow, putRow), segment)
     return board
